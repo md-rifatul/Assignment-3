6f687575af514126054819b7117cc2180e007dae
Rifatul <rifatul.ism@gmail.com>
2025-12-01 16:43:34 +0600

    implement AddRateLimiting

--- Assignment-3/Controllers/HelloController.cs
+++ Assignment-3/Controllers/HelloController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Assignment_3.Services.IServices;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
 
 namespace Assignment_3.Controllers
 {
@@ -7,6 +9,18 @@ namespace Assignment_3.Controllers
     [ApiController]
     public class HelloController : ControllerBase
     {
+        private readonly IHelloSerivice _helloSerivice;
+        public HelloController(IHelloSerivice helloSerivice)
+        {
+            _helloSerivice = helloSerivice;
+        }
 
+        [EnableRateLimiting("Fixed")]
+        [HttpGet("hello")]
+        public IActionResult GetHello()
+        {
+            var message = _helloSerivice.GetMessage();
+            return Ok(message);
+        }
     }
 }
--- Assignment-3/Program.cs
+++ Assignment-3/Program.cs
@@ -1,3 +1,7 @@
+using Assignment_3.Services;
+using Assignment_3.Services.IServices;
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//register service(DI)
+builder.Services.AddScoped<IHelloSerivice, HelloService>();
+
+
+//Rate Limiting
+builder.Services.AddRateLimiter(rate =>
+{
+    rate.AddFixedWindowLimiter(policyName: "Fixed", options =>
+    {
+        options.PermitLimit = 3;
+        options.Window = TimeSpan.FromSeconds(10);
+        options.QueueLimit = 0;
+        options.AutoReplenishment = true;
+    });
+    rate.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+});
+
 
 var app = builder.Build();
 
@@ -19,6 +41,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseRateLimiter();
 
 app.MapControllers();
 
